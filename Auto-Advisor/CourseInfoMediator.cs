9f6d61bda3dbba602850a0d1f9884996b7e4cd84
stormyflyby <montgomeryc1014@gmail.com>
2025-04-02 20:37:52 -0500

    Created a course info storage class

---
+++ Auto-Advisor/CourseInfoMediator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto_Advisor
+{
+    /* Stores course info given by user for other parts of the system to use
+     * To use, refer to CourseInfoMediator.Instance.
+     * For example, to add a major, set CourseInfoMediator.Instance.Major0.
+     */
+    internal sealed class CourseInfoMediator
+    {
+        // CourseInfoMediator is a singleton. This code ensures only one instance of it can exist.
+        // To use it, refer to CourseInfoMediator.Instance
+        private static readonly Lazy<CourseInfoMediator> instance = new Lazy<CourseInfoMediator>(() => new CourseInfoMediator());
+        public static CourseInfoMediator Instance => instance.Value;
+
+        public string Major0 { get; set; }
+        public string Major1 { get; set; }
+        public bool Honors { get; set; }
+        public short SemesterNumber { get; set; }
+        public List<string> CompletedCourses { get; }
+        public List<string> InProgressCourses { get; }
+
+
+
+
+        private CourseInfoMediator()
+        {
+            Major0 = string.Empty;
+            Major1 = string.Empty;
+            Honors = false;
+            SemesterNumber = 0;
+            CompletedCourses = new List<string>();
+            InProgressCourses = new List<string>();
+        }
+
+        public void AddCompletedCourse(string course)
+        {
+            CompletedCourses.Add(course);
+        }
+
+        public void RemoveCompletedCourse(string course)
+        {
+            CompletedCourses.Remove(course);
+        }
+
+        public void ClearCompletedCourses()
+        {
+            CompletedCourses.Clear();
+        }
+
+        public void AddInProgressCourse(string course)
+        {
+            InProgressCourses.Add(course);
+        }
+
+        public void RemoveInProgressCourse(string course)
+        {
+            InProgressCourses.Remove(course);
+        }
+
+        public void ClearInProgressCourses()
+        {
+            InProgressCourses.Clear();
+        }
+    }
+}
--- Auto-Advisor/obj/Debug/net7.0-windows/Auto-Advisor.AssemblyInfo.cs
+++ Auto-Advisor/obj/Debug/net7.0-windows/Auto-Advisor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Auto-Advisor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22c9db75798b9eb317cfc1a85aa72164794d382e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Auto-Advisor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Auto-Advisor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
