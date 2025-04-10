d33fb98f2f8db11f8e451f3f8a7e3e9885790b11
justinlindtx <justinlindtx@gmail.com>
2025-04-09 20:07:27 -0500

    Restructured how hours are calculated

--- Auto-Advisor/Form1.cs
+++ Auto-Advisor/Form1.cs
@@ -10,8 +10,8 @@ namespace Auto_Advisor
         string removedMajor = null;
         string removedMinor = null;
         int TotalHours = 0;
-        int TotalHoursStillNeeded = 126;
-        int TotalHoursNeeded = 126;
+        int TotalHoursStillNeeded = 0;
+        int TotalHoursNeeded = 0;
         public Form1()
         {
             InitializeComponent();
@@ -47,20 +47,15 @@ namespace Auto_Advisor
                 row.Cells[1].Value = course.name;
                 row.Cells[2].Value = course.hours;
 
+                // Update the total hours for the given major
+                TotalHoursNeeded += course.hours;
+
                 // Determine color of row
                 if (courseInTextBox(course.code, textBox1)) // Course already taken
                 {
                     row.DefaultCellStyle.BackColor = Color.LightGreen;
                     row.DefaultCellStyle.SelectionBackColor = Color.LightGreen;
                     TotalHours += course.hours;
-                    listBox6.Items.Clear();
-                    listBox6.Items.Add(TotalHours);
-                    TotalHoursStillNeeded -= TotalHours;
-                    listBox5.Items.Clear();
-                    listBox5.Items.Add(TotalHoursStillNeeded);
-                    listBox2.Items.Clear();
-                    listBox2.Items.Add(TotalHoursNeeded);
-
                 }
                 else if (courseInTextBox(course.code, textBox2)) // Course currently being taken
                 {
@@ -244,9 +239,6 @@ namespace Auto_Advisor
             {
                 majorDisplay.Items.Add(MinorBox2.SelectedItem);
             }
-            // Populate other sidebar data
-            listBox4.Items.Clear();
-            listBox4.Items.Add(comboBox4.SelectedItem); // Load current semester
 
             // Populate all courses from JSON files
             LoadCoursesIntoGrid(Path.Combine(majorPath, "major_classes.json"), dataGridMajors);
@@ -254,6 +246,17 @@ namespace Auto_Advisor
             LoadCoursesIntoGrid(Path.Combine(majorPath, "General_Education.json"), dataGridGenEd);
             LoadCoursesIntoGrid(Path.Combine(majorPath, "Theology_Courses.json"), dataGridTheology);
             recommendedCourses(Path.Combine(majorPath, "recommended.json"), dataGridRecommended);
+            
+            // Populate other sidebar data
+            listBox4.Items.Clear();
+            listBox4.Items.Add(comboBox4.SelectedItem); // Load current semester
+            listBox6.Items.Clear();
+            listBox6.Items.Add(TotalHours); // Load total hours taken
+            TotalHoursStillNeeded = TotalHoursNeeded - TotalHours;
+            listBox5.Items.Clear();
+            listBox5.Items.Add(TotalHoursStillNeeded); // Load hours still needed
+            listBox2.Items.Clear();
+            listBox2.Items.Add(TotalHoursNeeded); // Load total hours for major
 
             // Load minor info if it exists, else delete it
             if (MinorBox.SelectedItem != null && MinorBox.SelectedItem.ToString() != "None")
